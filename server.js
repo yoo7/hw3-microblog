f0dc9804f5470459ca6f043cb426fad55c4e7753
yoo7 <ybcjin@ucdavis.edu>
2024-05-08 17:08:26 -0700

    filled in some to-do func

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-const session = require('express-session');
-const canvas = require('canvas');
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const session = require("express-session");
+const canvas = require("canvas");
+const fs = require("fs");
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Configuration and Setup
@@ -22,7 +23,7 @@ const PORT = 3000;
     
     1. toLowerCase:
        - Converts a given string to lowercase.
-       - Usage example: {{toLowerCase 'SAMPLE STRING'}} -> 'sample string'
+       - Usage example: {{toLowerCase "SAMPLE STRING"}} -> "sample string"
 
     2. ifCond:
        - Compares two values for equality and returns a block of content based on 
@@ -39,7 +40,7 @@ const PORT = 3000;
 // Set up Handlebars view engine with custom helpers
 //
 app.engine(
-    'handlebars',
+    "handlebars",
     expressHandlebars.engine({
         helpers: {
             toLowerCase: function (str) {
@@ -55,8 +56,8 @@ app.engine(
     })
 );
 
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Middleware
@@ -64,7 +65,7 @@ app.set('views', './views');
 
 app.use(
     session({
-        secret: 'oneringtorulethemall',     // Secret key to sign the session ID cookie
+        secret: "oneringtorulethemall",     // Secret key to sign the session ID cookie
         resave: false,                      // Don't save session if unmodified
         saveUninitialized: false,           // Don't create session until something stored
         cookie: { secure: false },          // True if using https. Set to false for development without https
@@ -75,15 +76,15 @@ app.use(
 // should be used in your template files. 
 // 
 app.use((req, res, next) => {
-    res.locals.appName = 'MicroBlog';
+    res.locals.appName = "MicroBlog";
     res.locals.copyrightYear = 2024;
-    res.locals.postNeoType = 'Post';
+    res.locals.postNeoType = "Post";
     res.locals.loggedIn = req.session.loggedIn || false;
-    res.locals.userId = req.session.userId || '';
+    res.locals.userId = req.session.userId || "";
     next();
 });
 
-app.use(express.static('public'));                  // Serve static files
+app.use(express.static("/public"));                 // Serve static files
 app.use(express.urlencoded({ extended: true }));    // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.json());                            // Parse JSON bodies (as sent by API clients)
 
@@ -95,58 +96,61 @@ app.use(express.json());                            // Parse JSON bodies (as sen
 // We pass the posts and user variables into the home
 // template
 //
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     const posts = getPosts();
     const user = getCurrentUser(req) || {};
-    res.render('home', { posts, user });
+    res.render("home", { posts, user });
 });
 
 // Register GET route is used for error response from registration
 //
-app.get('/register', (req, res) => {
-    res.render('loginRegister', { regError: req.query.error });
+app.get("/register", (req, res) => {
+    res.render("loginRegister", { regError: req.query.error });
 });
 
 // Login route GET route is used for error response from login
 //
-app.get('/login', (req, res) => {
-    res.render('loginRegister', { loginError: req.query.error });
+app.get("/login", (req, res) => {
+    res.render("loginRegister", { loginError: req.query.error });
 });
 
 // Error route: render error page
 //
-app.get('/error', (req, res) => {
-    res.render('error');
+app.get("/error", (req, res) => {
+    res.render("error");
 });
 
 // Additional routes that you must implement
 
 
-app.get('/post/:id', (req, res) => {
+app.get("/post/:id", (req, res) => {
     // TODO: Render post detail page
 });
-app.post('/posts', (req, res) => {
+app.post("/posts", (req, res) => {
     // TODO: Add a new post and redirect to home
+    res.redirect("home");
 });
-app.post('/like/:id', (req, res) => {
+app.post("/like/:id", (req, res) => {
     // TODO: Update post likes
+    // let id = req.params.id;
+    // Do a call to getCurrentUser()
 });
-app.get('/profile', isAuthenticated, (req, res) => {
+app.get("/profile", isAuthenticated, (req, res) => {
     // TODO: Render profile page
 });
-app.get('/avatar/:username', (req, res) => {
+app.get("/avatar/:username", (req, res) => {
     // TODO: Serve the avatar image for the user
 });
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
     // TODO: Register a new user
 });
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     // TODO: Login a user
 });
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
     // TODO: Logout the user
 });
-app.post('/delete/:id', isAuthenticated, (req, res) => {
+app.post("/delete/:id", isAuthenticated, (req, res) => {
     // TODO: Delete a post if the current user is the owner
 });
 
@@ -164,52 +168,71 @@ app.listen(PORT, () => {
 
 // Example data for posts and users
 let posts = [
-    { id: 1, title: 'Sample Post', content: 'This is a sample post.', username: 'SampleUser', timestamp: '2024-01-01 10:00', likes: 0 },
-    { id: 2, title: 'Another Post', content: 'This is another sample post.', username: 'AnotherUser', timestamp: '2024-01-02 12:00', likes: 0 },
+    { id: 1, title: "Sample Post", content: "This is a sample post.", username: "SampleUser", timestamp: "2024-01-01 10:00", likes: 0 },
+    { id: 2, title: "Another Post", content: "This is another sample post.", username: "AnotherUser", timestamp: "2024-01-02 12:00", likes: 0 },
 ];
 let users = [
-    { id: 1, username: 'SampleUser', avatar_url: undefined, memberSince: '2024-01-01 08:00' },
-    { id: 2, username: 'AnotherUser', avatar_url: undefined, memberSince: '2024-01-02 09:00' },
+    { id: 1, username: "SampleUser", avatar_url: undefined, memberSince: "2024-01-01 08:00" },
+    { id: 2, username: "AnotherUser", avatar_url: undefined, memberSince: "2024-01-02 09:00" },
 ];
+// TODO: add more posts and ids
 
 // Function to find a user by username
 function findUserByUsername(username) {
-    // TODO: Return user object if found, otherwise return undefined
+    // Return user object if exists, otherwise return undefined
+    // For each user in users, check if the username matches the given one
+    return users.find(user => user.username === username);
 }
 
 // Function to find a user by user ID
 function findUserById(userId) {
-    // TODO: Return user object if found, otherwise return undefined
+    // Return user object if found, otherwise return undefined
+    return users.find(user => user.id === userId);
 }
 
 // Function to add a new user
 function addUser(username) {
-    // TODO: Create a new user object and add to users array
+    // Add new user object to the users array
+    // TODO do we use the first unavailable ID number? Since you can delete users...
+    const id = users[users.length - 1].id + 1;
+
+    users[users.length] = { id: id, username: username, avatar_url: undefined, memberSince: "2024-01-01 10:00" };
 }
 
 // Middleware to check if user is authenticated
 function isAuthenticated(req, res, next) {
     console.log(req.session.userId);
+
     if (req.session.userId) {
         next();
     } else {
-        res.redirect('/login');
+        res.redirect("/login");
     }
 }
 
 // Function to register a user
 function registerUser(req, res) {
+    let qObj = req.query;
+    addUser(req.query.username);  // TODO: Idk if this is how it works, but just based on palindrome example
+
     // TODO: Register a new user and redirect appropriately
+    res.redirect("home");
 }
 
 // Function to login a user
 function loginUser(req, res) {
+    let qObj = req.query;
+    // TODO maybe a global var to keep track of current user?
+
     // TODO: Login a user and redirect appropriately
+    res.redirect("home");
 }
 
 // Function to logout a user
 function logoutUser(req, res) {
     // TODO: Destroy session and redirect appropriately
+
+    res.redirect("/");
 }
 
 // Function to render the profile page
@@ -224,7 +247,7 @@ function updatePostLikes(req, res) {
 
 // Function to handle avatar generation and serving
 function handleAvatar(req, res) {
-    // TODO: Generate and serve the user's avatar image
+    // TODO: Generate and serve the user"s avatar image
 }
 
 // Function to get the current user from session
@@ -244,6 +267,28 @@ function addPost(title, content, user) {
 
 // Function to generate an image avatar
 function generateAvatar(letter, width = 100, height = 100) {
+    // TODO: Choose a color scheme based on the letter
+    const color = "#FF927A";  // TODO: placeholder
+    
+    // Create a canvas with specified dimensions
+    const canvas = createCanvas(width, height);
+    // Get a CanvasRenderingContext2D object
+    const context = canvas.getContent("2d");
+    
+
+    // Fill the entire rectangle (starting at (0, 0)) with the given color
+    context.fillStyle = color;
+    context.fillRect(0, 0, width, height);
+
+    // Draw the letter in the center
+    context.fillText(letter, width / 2, height / 2);
+
+    // TODO: Return the avatar as a PNG buffer
+    // TODO used this https://blog.logrocket.com/creating-saving-images-node-canvas/ but have to check
+    const buffer = canvas.toBuffer("images/png");
+    fs.writeFileSync("./image.png", buffer);
+
+
     // TODO: Generate an avatar image with a letter
     // Steps:
     // 1. Choose a color scheme based on the letter
